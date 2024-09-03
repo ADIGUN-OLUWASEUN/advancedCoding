dcb2ea4fe63de2d2e0792185b97c7ad3259a3f81
ADIGUN OLUWASEUN <144284957+ADIGUN-OLUWASEUN@users.noreply.github.com>
2024-09-03 21:49:26 +0100

    Add files via upload

---
+++ math/math.js
@@ -0,0 +1,59 @@
+// // math object - allows you to perform mathematical task on numbers and provides constants
+
+// // math properties(constants)
+// pi
+// console.log(Math.PI);
+
+// square root of 2
+// console.log(Math.SQRT2);
+
+// square root of 1/2
+// console.log(Math.SQRT1_2);
+
+// euler Number
+// console.log(Math.E);
+
+// natural log of 10
+// console.log(Math.LN10);
+
+
+// // math methods
+// round - round to the nearest integer
+// console.log(Math.round(3,7));
+// console.log(Math.round(4.4));
+// console.log(Math.round(4.5));
+
+// floor - round down to te nearest integer
+// console.log(Math.floor(4.7));
+// console.log(Math.floor(5.1));
+
+// ceil - round up to the nearest integer
+// console.log(Math.ceil(5.4));
+// console.log(Math.ceil(7.7));
+
+// trunc - returns the integer part
+// console.log(Math.trunc(10.7));
+// console.log(Math.trunc(20.5));
+
+// pow 2^5
+// console.log(Math.pow(2, 5));
+
+// //random - returns a floating number between 0(inclusive) and 1(exclusive)
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10));
+// console.log(Math.floor(Math.random() * 100) + 1);
+// console.log(Math.floor(Math.random() * 90) + 10);
+
+// console.log(Math.floor(Math.random() * (max - min) + min));
+
+// sqrt(x) - return the square root of x
+// console.log(Math.sqrt(4));
+
+// abs(x) - return the absolute(positive) value of x
+// console.log(Math.abs(-5));
+
+// max - return the maximum number in a a range of numbers
+// console.log(Math.max(1, 2, 45, 72, 2, 4, 57));
+// min - return the minimum number in a range of numbers
+// console.log(Math.min(1,2,45,72,2));
